4af7df0eb197cfbdfbdce0279c18d3b8b754c016
Gabriela Xavier <gabrielaxavierst1@gmail.com>
2023-01-13 18:36:33 -0300

    Criação primeira classe: Position

---
+++ src/application/Program.java
@@ -0,0 +1,10 @@
+package application;
+
+import boardgame.Position;
+
+public class Program {
+    public static void main(String[] args) {
+        Position p1 = new Position(6,5);
+        System.out.println(p1);
+    }
+}
